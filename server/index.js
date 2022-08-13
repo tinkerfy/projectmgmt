2aa681a5156ff1368ae2cad1cf1a4b12bde9d758
nadrolf <nadrolf13@gmail.com>
2022-08-13 13:09:50 +0800

    setup serve

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const cors = require('cors')
@@ -14,12 +15,20 @@ connectDB()
 
 app.use(cors())
 
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: process.env.NODE_ENV === 'development',
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'))
   })
-)
+}
+
+// app.use(
+//   '/graphql',
+//   graphqlHTTP({
+//     schema,
+//     graphiql: process.env.NODE_ENV === 'development',
+//   })
+// )
 
 app.listen(port, console.log(`Server running on port ${port}`))
